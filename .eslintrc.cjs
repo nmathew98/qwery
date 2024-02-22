9320446e0a073340de9db8e19128cf38dfc7662a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-22 18:10:09 +1300

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+		"prettier",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh", "prettier"],
+	rules: {
+		"prettier/prettier": ["error", { usePrettierrc: true }],
+		"@typescript-eslint/no-explicit-any": 0,
+		eqeqeq: ["error", "always"],
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+	},
+};
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+// import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), dts({ rollupTypes: true })],
+	// build: {
+	// 	lib: {
+	// 		entry: resolve("src", "index.ts"),
+	// 		name: "React Qwery",
+	// 	},
+	// },
+});
