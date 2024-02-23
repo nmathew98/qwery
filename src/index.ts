e05c49584ae1c580554aae2c54dffc269110c863
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-24 07:21:56 +1300

    chore: cleanup and update vite to build as lib (#14)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { createTransaction } from "@b.s/txn";
 export { makeRetry, makeMonitoredFetch, diff } from "@b.s/incremental";
 
 export * from "./context";
+export * from "./use-qwery";
+export * from "./use-remember-scroll";
