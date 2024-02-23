81747cc14a5fecb617bee1547e08e47e0c81fa82
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-24 05:17:38 +1300

    types: useQweryOptions (#9)
    * types: rough types for useQweryOptions
    
    * fix: with makeMonitoredFetch, if a fetch is not enabled then it returns void so don't initialize the crdt in this case. update useRememberScroll to not execute server side

--- src/use-remember-scroll/index.tsx
+++ src/use-remember-scroll/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React from "react";
 import {
 	ExecutionEnvironment,
 	useExecutionEnvironment,
@@ -7,7 +7,11 @@ import {
 export const useRememberScroll = () => {
 	const { executionEnvironment } = useExecutionEnvironment();
 
-	useEffect(() => {
+	React.useEffect(() => {
+		if (executionEnvironment !== ExecutionEnvironment.Browser) {
+			return;
+		}
+
 		const onScroll = () => {
 			const currentPath = window.location.pathname;
 
@@ -35,9 +39,7 @@ export const useRememberScroll = () => {
 			top: savedScroll.scrollY,
 		});
 
-		if (executionEnvironment === ExecutionEnvironment.Browser) {
-			window.addEventListener("scroll", onScroll);
-		}
+		window.addEventListener("scroll", onScroll);
 
 		return () => {
 			window.removeEventListener("scroll", onScroll);
