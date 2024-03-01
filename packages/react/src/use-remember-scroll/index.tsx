cdb93c9f00f22d579c8361d14b9d6a477303bb27
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-01 18:31:03 +1300

    chore: monorepo (#56)
    * chore: monorepo
    
    * chore: update github actions
    
    * chore: lint in ci/cd
    
    * Revert "chore: lint in ci/cd"
    
    This reverts commit 567b07e5cb6d29fefc5926e311923b7f8d63080c.
    
    * chore: lint in ci/cd

--- packages/react/src/use-remember-scroll/index.tsx
+++ packages/react/src/use-remember-scroll/index.tsx
@@ -44,5 +44,5 @@ export const useRememberScroll = () => {
 		return () => {
 			window.removeEventListener("scroll", onScroll);
 		};
-	}, []);
+	}, []); /* eslint react-hooks/exhaustive-deps: "off" */
 };
