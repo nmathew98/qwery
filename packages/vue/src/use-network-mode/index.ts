151af2508a0ffbf70dcf1ec62fbcb9b97f783d91
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-02 16:43:49 +1300

    chore(vue): use network mode

---
+++ packages/vue/src/use-network-mode/index.ts
@@ -0,0 +1,42 @@
+import { onMounted, onUnmounted, onUpdated, ref } from "vue";
+
+export enum NetworkMode {
+	Offline = 0,
+	Online,
+}
+
+export const useNetworkMode = ({
+	ping = "https://captive.apple.com/hotspot-detect.html",
+}) => {
+	const isConnected = ref(true);
+
+	const isOnline = () => {
+		isConnected.value = true;
+	};
+
+	const isOffline = () => {
+		isConnected.value = false;
+	};
+
+	onMounted(() => {
+		window.addEventListener("online", isOnline);
+		window.addEventListener("offline", isOffline);
+	});
+
+	onUpdated(() => {
+		fetch(ping).then(isOnline).catch(isOffline);
+	});
+
+	onUnmounted(() => {
+		window.removeEventListener("online", isOnline);
+		window.removeEventListener("offline", isOffline);
+	});
+
+	return {
+		connectionStatus: isConnected
+			? NetworkMode.Online
+			: NetworkMode.Offline,
+		isOnline,
+		isOffline,
+	};
+};
