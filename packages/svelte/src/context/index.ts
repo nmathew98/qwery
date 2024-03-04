129aa428d5eba13fb930bf75b0ec12137a0d9998
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-04 18:21:18 +1300

    feat(svelte): context (#114)

---
+++ packages/svelte/src/context/index.ts
@@ -0,0 +1,31 @@
+import { setContext, getContext, hasContext } from "svelte";
+import {
+	createCacheProvider,
+	type CacheProvider,
+	type CacheStore,
+} from "@b.s/incremental";
+import {
+	ExecutionEnvironment,
+	useExecutionEnvironment,
+} from "../use-execution-environment";
+import { createNoOpCache } from "./no-op-cache";
+
+export const QweryContext = Symbol("QweryContext");
+
+export const setQweryContext = (store?: CacheStore) => {
+	const { executionEnvironment } = useExecutionEnvironment();
+
+	if (executionEnvironment === ExecutionEnvironment.Server && !store) {
+		return setContext(QweryContext, createCacheProvider(createNoOpCache()));
+	}
+
+	return setContext(QweryContext, createCacheProvider(store));
+};
+
+export const useQweryContext = () => {
+	if (!hasContext(QweryContext)) {
+		return;
+	}
+
+	return getContext<CacheProvider>(QweryContext);
+};
