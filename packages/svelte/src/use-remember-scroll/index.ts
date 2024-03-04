d56d05f67d71498166cfa803fae1d0c52e7dc63a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-04 18:32:56 +1300

    feat(svelte): useRememberScroll (#116)

---
+++ packages/svelte/src/use-remember-scroll/index.ts
@@ -0,0 +1,46 @@
+import { onMount, onDestroy } from "svelte";
+import { ExecutionEnvironment } from "@b.s/qwery-shared";
+import { useExecutionEnvironment } from "../use-execution-environment";
+
+export const useRememberScroll = () => {
+	const { executionEnvironment } = useExecutionEnvironment();
+
+	if (executionEnvironment !== ExecutionEnvironment.Browser) {
+		return;
+	}
+
+	const onScroll = () => {
+		const currentPath = window.location.pathname;
+
+		sessionStorage.setItem(
+			currentPath,
+			JSON.stringify({
+				scrollX: window.scrollX,
+				scrollY: window.scrollY,
+			}),
+		);
+	};
+
+	onMount(() => {
+		const currentPath = window.location.pathname;
+
+		const savedScroll = sessionStorage.getItem(currentPath)
+			? JSON.parse(sessionStorage.getItem(currentPath) as string)
+			: null;
+
+		if (!savedScroll) {
+			return;
+		}
+
+		window.scrollTo({
+			left: savedScroll.scrollX,
+			top: savedScroll.scrollY,
+		});
+
+		window.addEventListener("scroll", onScroll);
+	});
+
+	onDestroy(() => {
+		window.removeEventListener("scroll", onScroll);
+	});
+};
