232927e8a56cde281cf4dad9047e95c803834196
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-04 18:30:39 +1300

    feat(svelte): useNetworkMode (#115)

---
+++ packages/svelte/src/use-network-mode/index.ts
@@ -0,0 +1,36 @@
+import { NetworkMode, type UseNetworkModeOptions } from "@b.s/qwery-shared";
+import { beforeUpdate, onDestroy, onMount } from "svelte";
+import { derived, writable } from "svelte/store";
+
+export const useNetworkMode = ({
+	ping = "https://captive.apple.com/hotspot-detect.html",
+}: UseNetworkModeOptions) => {
+	const isConnected = writable(true);
+
+	const isOnline = () => isConnected.set(true);
+	const isOffline = () => isConnected.set(false);
+
+	fetch(ping).then(isOnline).catch(isOffline);
+
+	onMount(() => {
+		window.addEventListener("online", isOnline);
+		window.addEventListener("offline", isOffline);
+	});
+
+	beforeUpdate(() => {
+		fetch(ping).then(isOnline).catch(isOffline);
+	});
+
+	onDestroy(() => {
+		window.removeEventListener("online", isOnline);
+		window.removeEventListener("offline", isOffline);
+	});
+
+	return {
+		connectionStatus: derived(isConnected, $isConnected =>
+			$isConnected ? NetworkMode.Online : NetworkMode.Offline,
+		),
+		isOnline,
+		isOffline,
+	};
+};
