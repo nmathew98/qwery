b0587f9d985e18236a95305d945d6bbf865c4ca7
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-04 20:31:31 +1300

    fix(svelte): for dispatch move subscribe out of getter

--- packages/svelte/src/use-qwery/index.ts
+++ packages/svelte/src/use-qwery/index.ts
@@ -36,14 +36,14 @@ export const useQwery = <
 	const crdt = writable<{
 		data: Data | null;
 		versions: Data[] | null;
-		dispatch: Dispatch<Data> | null;
+		dispatch: Dispatch<InferData<I>> | null;
 	}>({
 		data: null,
 		versions: null,
 		dispatch: null,
 	});
 
-	const updateCRDT = (value: CRDT<any>) => {
+	const updateCRDT = (value: CRDT<InferData<I>>) => {
 		crdt.set({
 			data: value.data,
 			versions: value.versions,
@@ -282,16 +282,16 @@ export const useQwery = <
 		})) as any;
 	}
 
+	let dispatch: Dispatch<InferData<I>> | null = null;
+	const unsubscribe = crdt.subscribe($crdt => {
+		if ($crdt.dispatch) {
+			dispatch = $crdt.dispatch;
+		}
+	});
+
 	return {
 		data: derived(crdt, $crdt => $crdt.data ?? computeInitialValue()),
 		get dispatch() {
-			let dispatch: Dispatch<Data> | null = null;
-			const unsubscribe = crdt.subscribe($crdt => {
-				if ($crdt.dispatch) {
-					dispatch = $crdt.dispatch;
-				}
-			});
-
 			if (!dispatch) {
 				return noOpFunction;
 			}
