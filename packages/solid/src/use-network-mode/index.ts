c07240edaf89c6af7b1d2053d82ada2a8ea4226e
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-07 19:29:05 +1300

    feat(solid): useNetworkMode

---
+++ packages/solid/src/use-network-mode/index.ts
@@ -0,0 +1,36 @@
+import { createSignal, onMount } from "solid-js";
+import { NetworkMode, type UseNetworkModeOptions } from "@b.s/qwery-shared";
+export { NetworkMode } from "@b.s/qwery-shared";
+
+export const useNetworkMode = ({
+	ping = "https://captive.apple.com/hotspot-detect.html",
+}: UseNetworkModeOptions) => {
+	const [isConnected, setIsConnected] = createSignal(true);
+	const isOnline = () => setIsConnected(true);
+	const isOffline = () => setIsConnected(false);
+
+	fetch(ping).then(isOnline).catch(isOffline);
+
+	onMount(() => {
+		const onOnline = () => setIsConnected(true);
+		const onOffline = () => setIsConnected(false);
+
+		window.addEventListener("online", onOnline);
+		window.addEventListener("offline", onOffline);
+
+		return () => {
+			window.removeEventListener("online", onOnline);
+			window.removeEventListener("offline", onOffline);
+		};
+	});
+
+	return {
+		connectionStatus: () => {
+			fetch(ping).then(isOnline).catch(isOffline);
+
+			return isConnected() ? NetworkMode.Online : NetworkMode.Offline;
+		},
+		isOnline,
+		isOffline,
+	};
+};
