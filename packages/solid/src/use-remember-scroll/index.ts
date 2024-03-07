320e8b790a07d18515681a26c808adfe6ce60f5b
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-07 19:21:21 +1300

    feat(solid): useRememberScroll (#140)

---
+++ packages/solid/src/use-remember-scroll/index.ts
@@ -0,0 +1,46 @@
+import { onMount } from "solid-js";
+import { ExecutionEnvironment } from "@b.s/qwery-shared";
+import { useExecutionEnvironment } from "../use-execution-environment";
+
+export const useRememberScroll = () => {
+	const { executionEnvironment } = useExecutionEnvironment();
+
+	onMount(() => {
+		if (executionEnvironment !== ExecutionEnvironment.Browser) {
+			return;
+		}
+
+		const onScroll = () => {
+			const currentPath = window.location.pathname;
+
+			sessionStorage.setItem(
+				currentPath,
+				JSON.stringify({
+					scrollX: window.scrollX,
+					scrollY: window.scrollY,
+				}),
+			);
+		};
+
+		const currentPath = window.location.pathname;
+
+		const savedScroll = sessionStorage.getItem(currentPath)
+			? JSON.parse(sessionStorage.getItem(currentPath) as string)
+			: null;
+
+		if (!savedScroll) {
+			return;
+		}
+
+		window.scrollTo({
+			left: savedScroll.scrollX,
+			top: savedScroll.scrollY,
+		});
+
+		window.addEventListener("scroll", onScroll);
+
+		return () => {
+			window.removeEventListener("scroll", onScroll);
+		};
+	});
+};
