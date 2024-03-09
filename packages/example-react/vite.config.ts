bac49ae922f8592f4526438cfa0166d8f2bcbbdc
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-09 13:31:16 +1300

    chore(example-react): add shadcn/ui

--- packages/example-react/vite.config.ts
+++ packages/example-react/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
