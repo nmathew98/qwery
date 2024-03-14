e08db0ad47e48d74a3003a5afa6e005e4a23bfc5
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-14 18:32:23 +1300

    refactor(react,vue,svelte,solid): move useRememberScroll core to shared

--- packages/react/src/use-remember-scroll/index.ts
+++ packages/react/src/use-remember-scroll/index.ts
@@ -1,41 +1,16 @@
 import React from "react";
-import { isBrowser } from "@b.s/qwery-shared";
+import { makeOnScroll, restoreScroll } from "@b.s/qwery-shared";
 
 export const useRememberScroll = () => {
 	React.useEffect(() => {
-		if (!isBrowser()) {
-			return;
-		}
+		restoreScroll();
 
-		const currentPath = window.location.pathname;
-
-		const savedScroll = sessionStorage.getItem(currentPath)
-			? JSON.parse(sessionStorage.getItem(currentPath) as string)
-			: null;
-
-		if (savedScroll) {
-			window.scrollTo({
-				left: savedScroll.scrollX,
-				top: savedScroll.scrollY,
-			});
-		}
-
-		const onScroll = () => {
-			const currentPath = window.location.pathname;
-
-			sessionStorage.setItem(
-				currentPath,
-				JSON.stringify({
-					scrollX: window.scrollX,
-					scrollY: window.scrollY,
-				}),
-			);
-		};
+		const onScroll = makeOnScroll();
 
 		window.addEventListener("scroll", onScroll);
 
 		return () => {
 			window.removeEventListener("scroll", onScroll);
 		};
-	}, []); /* eslint react-hooks/exhaustive-deps: "off" */
+	}, []);
 };
