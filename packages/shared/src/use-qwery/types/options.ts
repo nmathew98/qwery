6d0f695adc56e443ec1a90c1b95020ad27639621
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-03-17 11:25:50 +1300

    chore(react,vue,solid,svelte): update incremental

--- packages/shared/src/use-qwery/types/options.ts
+++ packages/shared/src/use-qwery/types/options.ts
@@ -5,30 +5,11 @@ import type {
 } from "@b.s/incremental";
 import type { BaseUseQweryOptions } from "./base";
 
-export type UseQweryUntaggedOptions<
-	I extends InitialValue,
-	S extends boolean | undefined,
-	DInferred extends Data = I extends () => Promise<
-		infer DInferred extends Data
-	>
-		? DInferred
-		: I,
-> = BaseUseQweryOptions<S, DInferred> &
-	Pick<
-		CreateCRDTParameters<DInferred>,
-		"onChange" | "onSuccess" | "onError"
-	> & {
-		initialValue: I;
-		subscribe?: (
-			dispatch: Dispatch<DInferred>,
-		) => Promise<(() => void) | void> | (() => void) | void;
-	};
-
 export interface DispatchOptionsInternal extends DispatchOptions {
 	isBroadcasted?: boolean;
 }
 
-export type UseQweryTaggedOptions<
+export type UseQweryOptions<
 	I extends InitialValue,
 	S extends boolean | undefined,
 	DInferred extends Data = I extends () => Promise<
@@ -38,7 +19,7 @@ export type UseQweryTaggedOptions<
 		: I,
 > = BaseUseQweryOptions<S, DInferred> &
 	Pick<
-		CreateCRDTParameters<DInferred>,
+		CreateCRDTParameters<DInferred, any>,
 		"onChange" | "onSuccess" | "onError"
 	> & {
 		initialValue?: I;
@@ -46,11 +27,6 @@ export type UseQweryTaggedOptions<
 			dispatch: Dispatch<DInferred>,
 		) => Promise<(() => void) | void> | (() => void) | void;
 	};
-
-export type UseQweryOptions<
-	I extends InitialValue,
-	S extends boolean | undefined,
-> = UseQweryUntaggedOptions<I, S> | UseQweryTaggedOptions<I, S>;
 
 export type Data = Record<string | number | symbol, any> | Array<any>;
 
